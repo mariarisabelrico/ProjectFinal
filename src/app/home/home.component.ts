bc4e95a17b68cb6f531af400b34e6af690eb87cb
Julia SM <cienvariables@gmail.com>
2022-07-22 19:48:02 +0100

    mmm

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,24 +5,24 @@ import { AnimationOptions } from 'ngx-lottie';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
 
-  options:AnimationOptions ={
-    path:'https://assets5.lottiefiles.com/packages/lf20_slckthsz.json'
+  options: AnimationOptions = {
+    path: 'https://assets5.lottiefiles.com/packages/lf20_slckthsz.json'
   }
 
-  options02:AnimationOptions ={
-    path:'https://assets9.lottiefiles.com/packages/lf20_djntqslv.json'
+  options02: AnimationOptions = {
+    path: 'https://assets9.lottiefiles.com/packages/lf20_djntqslv.json'
   }
 
-  options03:AnimationOptions ={
-    path:'https://assets1.lottiefiles.com/packages/lf20_3qh85mab.json'
+  options03: AnimationOptions = {
+    path: 'https://assets1.lottiefiles.com/packages/lf20_3qh85mab.json'
   }
 
-  options04:AnimationOptions ={
-    path:'https://assets2.lottiefiles.com/packages/lf20_ns1snx0q.json'
+  options04: AnimationOptions = {
+    path: 'https://assets2.lottiefiles.com/packages/lf20_ns1snx0q.json'
   }
   constructor() { }
 
