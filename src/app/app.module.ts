3109a0452a382b17997c14e927a4b031062c6423
mariarisabelrico <rico.pereira@bootcamp-upgrade.com>
2022-07-22 20:24:27 +0200

    header y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { HeaderComponent } from './core/components/header/header.component';
 
 export function playerFactory() {
   return player;
 }
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent,FooterComponent,HeaderComponent],
 
   imports: [
     BrowserModule,
