0e125c84470b8931b0f428b62bbfc225db016bdd
Julia SM <cienvariables@gmail.com>
2022-07-24 19:31:51 +0100

    Revert "header"
    This reverts commit f1fcc1d441d02d1b88b333f0747ad1f7f5679adc.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FooterComponent } from './core/components/footer/footer.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { LoginComponent } from './pages/login/login.component';
-<<<<<<< HEAD
 import { RegistroComponent } from './pages/registro/registro.component';
-=======
-import { RegisterComponent } from './pages/register/register.component';
->>>>>>> 5539668353d8f7969df9c483779da7c547d8c1c0
 
 export function playerFactory() {
   return player;
@@ -27,11 +23,7 @@ export function playerFactory() {
     LoginComponent,
     FooterComponent,
     HeaderComponent,
-<<<<<<< HEAD
     RegistroComponent
-=======
-    RegisterComponent,
->>>>>>> 5539668353d8f7969df9c483779da7c547d8c1c0
   ],
 
   imports: [
