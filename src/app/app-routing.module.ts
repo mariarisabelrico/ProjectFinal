541d1dce72c86a4e5206fdb9224a3465f5994c39
Julia SM <cienvariables@gmail.com>
2022-07-28 21:11:15 +0100

    more styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { LandingComponent } from './pages/landing/landing.component';
 import { HomeComponent } from './pages/homeUser/homeUser.component';
+import { LibraryComponent } from './pages/library/library.component';
+import { AjustesComponent } from './pages/ajustes/ajustes.component';
 
 const routes: Routes = [
   { path: '', component: LandingComponent },
   { path: 'Registro', component: RegistroComponent },
   { path: 'Login', component: LoginComponent },
   { path: 'HomeUser', component: HomeComponent },
+  { path: 'Library', component: LibraryComponent },
+  { path: 'Registro', component: RegistroComponent },
+  { path: 'Ajustes', component: AjustesComponent },
 ];
 
 @NgModule({
